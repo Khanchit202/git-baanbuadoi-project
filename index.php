4c558bda73f70d7ddebb830cc9d9654ec87bf6a0
Khanchit202 <khanchit202@gmail.com>
2024-07-21 21:20:01 +0700

    เปลี่ยน logo แก้ไขบางส่วน

--- index.php
+++ index.php
@@ -5,60 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="index.css">
-
-    <style>
-        @font-face {
-            font-family: kkfont;
-            src: url('FC Iconic Regular.ttf');
-        }
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: kkfont;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 
     <div class="container shadow-sm rounded bg-white" style="padding: 50px 30px; border-color: #ccc; border-width: 1px; border-radius: 4px; width: 1080px; height: 3000px;">
-        <nav style="position: sticky; top: 20px;">
+        <nav>
             <?php include("tabbar_view/tab_bar.php"); ?>
         </nav>
 
-        <div class="news-web">
-            <div class="image-news">
-            <img src="news_001.jpg" width="300px">
-            </div>
-            <div class="title">
-                <h2>ประชาสัมพันธ์</h2>
-                <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                <a href="#" class="btn btn-custom" style="padding-right:160px;padding-left:160px;">ดูเพิ่มเติม</a>
-            </div>
-            
+        <div id="title">
+            <h1>แนะนำ</h1>
+            <p>ห้องพัก บ้านพัก และบริการ</p>
         </div>
-
-        <div class="title">
-            <h1>ห้องพักและบริการ</h1>
-            <p>บ้านบัวดอยโฮมสเตย์ ดอยอ่างขาง จังหวัดเชียงใหม่</p>
-        </div>
-
-        <div class="menu-product">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-        </div>
-
-        <div class="title">
-            <h1>ห้องพักและบริการ</h1>
-            <p>บ้านบัวดอยโฮมสเตย์ ดอยอ่างขาง จังหวัดเชียงใหม่</p>
-        </div>
-        
-
-
-
 
     </div>
     
