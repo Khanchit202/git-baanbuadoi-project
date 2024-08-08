fa787f0ca90a1af06b349a480935d4538c595410
Hobbs002 <lakwazap@gmail.com>
2024-08-08 14:31:21 +0700

    แทบบาร์

--- tabbar_view/tab_bar.php
+++ tabbar_view/tab_bar.php
@@ -3,6 +3,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 ?>
 
 <div class="topbar_menu">
+<<<<<<< Updated upstream
     <div class="topbar_menu_logo">
         <a href="index.php"><img src="tabbar_view/baanbuadoi.png" alt="โฮมสเตย์บ้านบัวดอย Baanbuadoi"></a>
         <p>โฮมสเตย์บ้านบัวดอย</p>
@@ -28,3 +29,20 @@ $current_page = basename($_SERVER['PHP_SELF']);
         <a href=""><div class="btn btn-custom">เข้าสู่ระบบ</div></a>
     </div>
 </div>
+=======
+            <div class="topbar_menu_logo">
+                <a href="index.php"><img src="tabbar_view/baanbuadoi.png" alt="โฮมสเตย์บ้านบัวดอย Baanbuadoi"></a>
+                <p>โฮมสเตย์บ้านบัวดอย</p>
+            </div>
+            <div class="topbar_menu_list">
+                <ul>
+                    <li><a href='./index.php'>หน้าหลัก</a></li>
+                    <li><a href="">ข้อมูลห้องพัก</a></li>
+                    <li><a href="">ข้อมูลบริการ</a></li>
+                    <li><a href="">ข่าวสารประชาสัมพันธ์</a></li>
+                    <li><a href='./contect.php'>เกี่ยวกับเรา</a></li>
+                </ul>
+                <a href=""><div class="btn btn-custom">เข้าสู่ระบบ</div></a>
+            </div>
+        </div>
+>>>>>>> Stashed changes
