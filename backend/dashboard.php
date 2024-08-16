9f38d8600f7c98ce6a88cd9d823203f641286c83
Khanchit202 <khanchit202@gmail.com>
2024-08-16 22:59:58 +0700

    เพิ่มส่วน backend

---
+++ backend/dashboard.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Admin Dashboard | Korsat X Parmaga</title>
+    <link rel="stylesheet" href="dash.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <img src="../tabbar_view/baanbuadoi.png" style="margin-top: 10px; width: 40px; height: 40px;">
+                        </span>
+                        <span class="title">กิจการบ้านบัวดอย</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="../index.php">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+
+        <div class="main">
+            <div class="topbar">
+                <div class="toggle">
+                    <ion-icon name="menu-outline"></ion-icon>
+                </div>
+
+            </div>
+
+            
+            <div class="cardBox">
+                
+                
+            </div>
+
+            <div class="details">
+
+            </div>    
+        </div>
+    </div>
+
+    <script src="main.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    
+</body>
+
+</html>
