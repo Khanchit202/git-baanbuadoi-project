477a06684b3baf097698b731738cae25ed03750f
Khanchit202 <khanchit202@gmail.com>
2024-08-17 20:00:46 +0700

    เริ่มระบบ backend

--- backend/dashboard.php
+++ backend/dashboard.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,59 +8,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Admin Dashboard | Korsat X Parmaga</title>
+    <title>Sidebar With Bootstrap</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="dash.css">
 </head>
 
 <body>
-    <div class="container">
-        <div class="navigation">
-            <ul>
-                <li>
-                    <a href="#">
-                        <span class="icon">
-                            <img src="../tabbar_view/baanbuadoi.png" style="margin-top: 10px; width: 40px; height: 40px;">
-                        </span>
-                        <span class="title">กิจการบ้านบัวดอย</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="../index.php">
-                        <span class="icon">
-                            <ion-icon name="home-outline"></ion-icon>
-                        </span>
-                        <span class="title">Dashboard</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-
-
-        <div class="main">
-            <div class="topbar">
-                <div class="toggle">
-                    <ion-icon name="menu-outline"></ion-icon>
+    <div class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex" style="padding: 20px 0 20px 20px;">
+                <img src="../tabbar_view/baanbuadoi.png" alt="" width="30px">
+                <div class="sidebar-logo">
+                    <a href="#">กิจการบ้านบัวดอย</a>
                 </div>
-
             </div>
 
+            <?php include("menu_dash.php"); 
+                menu_dash()
+            ?>
             
-            <div class="cardBox">
-                
-                
+            <div class="sidebar-footer">
+                <a href="../index.php" class="sidebar-link">
+                    <i class="lni lni-home"></i>
+                    <span>กลับหน้าหลัก</span>
+                </a>
             </div>
-
-            <div class="details">
-
-            </div>    
+        </aside>
+        <div class="main p-3">
+            <nav class="navbar navbar-light bg-light" style="border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);margin-bottom: 20px;">
+                <div class="bar">
+                    <button class="toggle-btn" type="button">
+                        <i class="lni lni-menu"></i>
+                    </button>
+                </div>
+            </nav>
+                
+            <div class="text-center">
+                <?php include("user_data\set_accessrights.php"); ?>
+            </div>
         </div>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
     <script src="main.js"></script>
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    
 </body>
 
 </html>
---
+++ backend/user_data/set_accessrights.php
@@ -0,0 +1,54 @@
+<div style="display: flex; justify-content: space-between;">
+    <div style="background-color: #f5f5f5; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); padding: 20px; width: 25%; display: flex; flex-direction: column; align-items: center;">
+        <div style="background-color: #e0e0e0; border-radius: 50%; width: 100px; height: 100px; margin-bottom: 15px;"></div>
+        <div>นายครรชิต บางพระ</div>
+        <div>ผู้ดูแลระบบ</div>
+        <button style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; text-align: center; margin-top: 20px;">รีเซตรหัสผ่าน</button>
+        <div>เมื่อ 01/12/2566</div>
+        <a href="#">รายชื่อผู้ใช้</a>
+    </div>
+
+
+    <div style="background-color: #f5f5f5; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); padding: 20px; width: 73%; display: flex; flex-direction: column;">
+        <div style="background-color: #e8d7d7; padding: 10px; border-radius: 5px; margin-bottom: 10px;">ชื่อผู้ใช้ : admin_khanchit01</div>
+        <div style="background-color: #e8d7d7; padding: 10px; border-radius: 5px; margin-bottom: 10px;">ชื่อ - นามสกุล : ครรชิต บางพระ</div>
+        <div style="background-color: #e8d7d7; padding: 10px; border-radius: 5px; margin-bottom: 10px;">สิทธิการเข้าถึง : ผู้ดูแลระบบ</div>
+        <div style="background-color: #e8d7d7; padding: 10px; border-radius: 5px; margin-bottom: 10px;">เบอร์โทรศัพท์ : 0958053137</div>
+        <div style="background-color: #e8d7d7; padding: 10px; border-radius: 5px; margin-bottom: 10px;">Email Address : Khanchit202@gmail.com</div>
+        <button style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; text-align: center; margin-top: 20px;">บันทึกข้อมูล</button>
+    </div>
+
+    
+
+</div>
+<div style="margin-top: 20px; background-color: #f5f5f5; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);padding: 20px;">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>
