05fc4db1f7b96241021619517621410a3f73e5c2
Khanchit202 <khanchit202@gmail.com>
2024-09-06 19:50:48 +0700

    แก้ไข้เพิ่ม

--- user/check_login.php
+++ user/check_login.php
@@ -1,4 +1,5 @@
 <header>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <link rel="stylesheet" href="../font-web.css">
 </header>
@@ -37,7 +38,10 @@ if ($row) {
             Swal.fire({
                 icon: 'success',
                 title: 'เข้าสู่ระบบสำเร็จ',
-                text: 'กำลังนำคุณไปยังหน้าแรก...',
+                html: '<button class=\"btn btn-success\" type=\"button\" disabled>' +
+                        '<span class=\"spinner-border spinner-border-sm\" role=\"status\" aria-hidden=\"true\"></span>' +
+                        ' กำลังนำท่านไปยังหน้าหลัก...' +
+                        '</button>',
                 timer: 2500,
                 showConfirmButton: false
             }).then(() => {
