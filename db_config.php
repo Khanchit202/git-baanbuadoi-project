ed86f70818fea950015b12709d99188f111ce45f
Khanchit202 <khanchit202@gmail.com>
2024-09-16 21:25:47 +0700

    set post

--- db_config.php
+++ db_config.php
@@ -5,22 +5,24 @@
             $db_host = "localhost";
             $db_user = "root";
             $db_pass = "";
-            $db_ports = "3306";
+            $db_ports = ["3307", "3306"];
         }else if($type=="server"){
             $db_name = "";
             $db_host = "";
             $db_user = "";
             $db_pass = "";
-            $db_ports = "";
+            $db_ports = [""];
         }
-        try {
-            $db_con = new PDO("mysql:host={$db_host}; dbname={$db_name}; port={$db_ports}",$db_user,$db_pass);
-            $db_con -> exec("set names utf8");
-        } catch (PDOException $e) {
-            echo $e -> getMessage();
+        foreach ($db_ports as $db_port) {
+            try {
+                $db_con = new PDO("mysql:host={$db_host}; dbname={$db_name}; port={$db_port}", $db_user, $db_pass);
+                $db_con->exec("set names utf8");
+                return $db_con;
+            } catch (PDOException $e) {
+                echo "Connection failed on port {$db_port}: " . $e->getMessage() . "<br>";
+            }
+        }
         
-            
+        return null;
     }
-    return $db_con;
-}
-    ?>
+?>
